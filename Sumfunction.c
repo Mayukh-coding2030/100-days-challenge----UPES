64ace3a812b64048958c591a1a5b8283f371b476
Mayukh Chatterjee <mayukhchatterjee40@gmail.com>
2026-09-06 10:55:24 +0530

    Add files via upload

---
+++ Sumfunction.c
@@ -0,0 +1,27 @@
+//Q1: Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a :");
+    scanf("%d",&a);
+    printf("Enter the value of b :");
+    scanf("%d",&b);
+    int sum;
+    sum = a + b;
+    printf(" sum = %d" ,sum);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
