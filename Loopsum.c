094e07e8a71dc7eb61a09f1b3ee08fd1734a96a3
Mayukh Chatterjee <mayukhchatterjee40@gmail.com>
2026-09-17 22:48:03 +0530

    Add files via upload

---
+++ Loopsum.c
@@ -0,0 +1,13 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+#include<stdio.h>
+int main(){
+    int i,n,sum=0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for( i=1;i<=n;i++){
+        sum = sum + (2 * i - 1);
+    }
+        printf(" Sum = %d",sum);
+    
+    return 0;
+}
