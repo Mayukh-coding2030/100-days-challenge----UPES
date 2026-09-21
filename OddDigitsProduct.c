b1b611b50e63918897a8ef173cc2cf2558f58e81
Mayukh Chatterjee <mayukhchatterjee40@gmail.com>
2026-09-22 00:23:39 +0530

    Add files via upload

---
+++ OddDigitsProduct.c
@@ -0,0 +1,16 @@
+//Q39: Write a program to find the product of odd digits of a number.
+#include<stdio.h>
+int main(){
+    int n,product=1,digit;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    while(n>0){
+        digit = n % 10;
+        if(digit%2!=0){
+            product = product * digit;
+        }
+        n = n / 10;
+    }
+    printf("%d",product);
+    return 0;
+}
